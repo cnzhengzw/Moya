9f2e8001dbe80fc68ba9e4bedf9666c6ddbfa6f7
cnzhengzw <92014984+cnzhengzw@users.noreply.github.com>
2022-06-14 17:01:46 +0800

    Update URL+Moya.swift
    url带问号重写

--- Sources/Moya/URL+Moya.swift
+++ Sources/Moya/URL+Moya.swift
@@ -10,6 +10,15 @@ public extension URL {
         let targetPath = target.path
         if targetPath.isEmpty {
             self = target.baseURL
+        }else if target.path.contains("?") {
+            // 这里直接强制解包了，这里可以修改，如果你需要。
+            let newPath = target.baseURL.absoluteString + target.path
+            let encodPath = newPath.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+            if let url = URL(string: encodPath) {
+                self = url
+            }else{
+                self = target.baseURL
+            }
         } else {
             self = target.baseURL.appendingPathComponent(targetPath)
         }
